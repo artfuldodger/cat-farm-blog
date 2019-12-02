10e38a8507725d7193439b76a64adeea7655f689
Jon Evans <joncodes+github@gmail.com>
2019-12-01 19:56:02 -0700

    Add Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_222323) do
+ActiveRecord::Schema.define(version: 2019_12_02_025508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2019_12_01_222323) do
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.bigint "author_id", null: false
     t.string "title"
@@ -45,5 +54,6 @@ ActiveRecord::Schema.define(version: 2019_12_01_222323) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "authors"
 end
